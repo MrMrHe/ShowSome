e7ceb70cc2e9443dd98089c14fcaf17123d43da3
1575645229@qq.com <1575645229@qq.com>
2020-11-27 16:57:51 +0800

    修改图片显示逻辑和网页显示

--- app/src/main/java/com/show/showsome/MainActivity.java
+++ app/src/main/java/com/show/showsome/MainActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -21,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mBinding;
 
     private boolean isWebViewShow = false;
+
+    private boolean isImageClick = false;
+
+    private String webUrl = "http://www.baidu.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +46,19 @@ public class MainActivity extends AppCompatActivity {
         mBinding.buttonImageVideo.setOnClickListener(listener);
         mBinding.buttonVideo.setOnClickListener(listener);
         mBinding.buttonVideoVideo.setOnClickListener(listener);
-
+        initWebView();
     }
 
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+    }
+
+
     private PerfectClickListener listener = new PerfectClickListener() {
-        @SuppressLint("NonConstantResourceId")
+        @SuppressLint({"NonConstantResourceId", "UseCompatLoadingForDrawables"})
         @Override
         protected void onNoDoubleClick(final View v) {
 
@@ -53,22 +70,18 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 case R.id.button_web:// 网页
                     show(mBinding.webView);
-                    //访问网页
-                    mBinding.webView.loadUrl("http://www.baidu.com");
-                    //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
-                    mBinding.webView.setWebViewClient(new WebViewClient() {
-                        @Override
-                        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                            //使用WebView加载显示url
-                            view.loadUrl(url);
-                            isWebViewShow = true;
-                            //返回true
-                            return true;
-                        }
-                    });
+                    isWebViewShow = true;
                     break;
                 case R.id.button_image:// 图片
                     show(mBinding.imageView);
+                    mBinding.imageViewArea.setVisibility(View.VISIBLE);
+                    if (!isImageClick) {
+                        mBinding.imageView.setImageDrawable(getDrawable(R.drawable.qr_code));
+                        isImageClick = true;
+                    } else {
+                        mBinding.imageView.setImageDrawable(getDrawable(R.drawable.long_image));
+                        isImageClick = false;
+                    }
 //                    mBinding.imageView.setImageDrawable();
                     break;
 
@@ -88,6 +101,19 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            //全屏播放退出全屏
+            if (mBinding.webView.canGoBack() && isWebViewShow) {
+                mBinding.webView.goBack();
+                return true;
+                //退出网页
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
     private void show(View view) {
         List<View> views = new ArrayList<View>();
         views.add(mBinding.textView);
@@ -101,12 +127,74 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 mView.setVisibility(View.GONE);
             }
-            if (isWebViewShow && !mView.equals(view)) {
-                mBinding.webView.onPause();
-            }
         }
-
+        isWebViewShow = false;
+        mBinding.imageViewArea.setVisibility(View.GONE);
     }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void initWebView() {
+        //访问网页
+        mBinding.webView.loadUrl(webUrl);
+        //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
+        mBinding.webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                try {
+                    if (url.startsWith("http:") || url.startsWith("https:")) {
+                        view.loadUrl(url);
+                    } else {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        startActivity(intent);
+                    }
+                } catch (Exception e) {
+                    return false;
+                }
+                //使用WebView加载显示url
+                view.loadUrl(url);
+                isWebViewShow = true;
+                //返回true
+                return true;
+            }
+        });
+        WebSettings ws = mBinding.webView.getSettings();
+        // 网页内容的宽度是否可大于WebView控件的宽度
+        ws.setLoadWithOverviewMode(false);
+        // 保存表单数据
+        ws.setSaveFormData(true);
+        // 是否应该支持使用其屏幕缩放控件和手势缩放
+        ws.setSupportZoom(true);
+        ws.setBuiltInZoomControls(true);
+        ws.setDisplayZoomControls(false);
+        // 启动应用缓存
+        ws.setAppCacheEnabled(true);
+        // 设置缓存模式
+        ws.setCacheMode(WebSettings.LOAD_DEFAULT);
+        // setDefaultZoom  api19被弃用
+        // 设置此属性，可任意比例缩放。
+        ws.setUseWideViewPort(true);
+        // 不缩放
+        mBinding.webView.setInitialScale(100);
+        // 告诉WebView启用JavaScript执行。默认的是false。
+        ws.setJavaScriptEnabled(true);
+        //  页面加载好以后，再放开图片
+        ws.setBlockNetworkImage(false);
+        // 使用localStorage则必须打开
+        ws.setDomStorageEnabled(true);
+        // 排版适应屏幕
+        ws.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        // WebView是否新窗口打开(加了后可能打不开网页)
+//        ws.setSupportMultipleWindows(true);
+
+        // webview从5.0开始默认不允许混合模式,https中不能加载http资源,需要设置开启。
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ws.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        /** 设置字体默认缩放大小(改变网页字体大小,setTextSize  api14被弃用)*/
+        ws.setTextZoom(100);
+    }
+
 
     @Override
     protected void onDestroy() {
